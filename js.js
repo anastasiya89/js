1a57643ddecc58514120ea4fd6dacd27774ba243
Anastasiya <anastasiya.garalova@gmail.com>
2020-01-28 21:40:36 +0200

    Это наш первый комит

---
+++ js.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// JavaScript Document
+/*let t=1;
+
+{
+	
+	let t=3;
+	console.log(t);
+}
+
+
+console.log(t);
+
+let person = {
+	name: "Jon",
+	age: "25",
+	isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr=['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr['2']);
+
+let calc = prompt("How old are you?", 18);
+let sum = calc + 10 / 2;
+console.log(typeof(sum));
+alert(sum);
+*/
+ q = 8;
+console.log(q);
