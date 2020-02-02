c0a886d798713c02a77849cbbcc31c3ba178a430
Anastasiya <anastasiya.garalova@gmail.com>
2020-02-02 12:33:55 +0200

    Task 1

---
+++ Task1/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+ 
+let	appData = {},
+	expensesi = {},
+	expenses1,
+	expenses2;
+
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+expenses1 = prompt("Введите обязательную статью расходов в этом месяце");
+expenses2 = prompt("Во сколько обойдется?");
+expensesi[expenses1] = expenses2;
+
+appData = {
+	moneySum: money,
+	timeData: time,
+	expenses: {[expenses1] : expenses2},
+	optionalExpenses: "",
+	income: [],
+	savings: false
+};
+
+console.log(appData);
+	
